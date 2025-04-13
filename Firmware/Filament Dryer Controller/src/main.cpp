05fcbb85806f6c2780f6d2368dbae183f85218a8
halojenproductions <40622702+halojenproductions@users.noreply.github.com>
2025-04-13 17:06:32 +1200

    Screen working.

--- Firmware/Filament Dryer Controller/src/main.cpp
+++ Firmware/Filament Dryer Controller/src/main.cpp
@@ -1,17 +1,54 @@
+#include <Adafruit_GFX.h>
 #include <Arduino.h>
-
+#include <u8g2lib.h> // Include the u8g2 library for graphics
 // put function declarations here:
 int myFunction(int, int);
 
+// Pins
+const int pButt = 2;	 // PD2
+const int pTemp = A0;	 // PC0
+const int pLedOk = 12;	 // PB4
+const int pLedHeat = 13; // PB5
+const int pHeater = 9;	 // PB1
+const int pFan = 5;	  // PD5 The PWM outputs generated on pins 5 and 6 will have
+					  // higher-than-expected duty cycles.
+const int pSda = SDA; // PC4
+const int pScl = SCL; // PC5
+
+// Screen
+#define SCREEN_WIDTH 128		// OLED display width, in pixels.
+#define SCREEN_HEIGHT 64		// OLED display height, in pixels.
+const int screenAddress = 0x78; //
+U8G2_SH1106_128X64_NONAME_2_SW_I2C u8g2(U8G2_R0, pScl, pSda);
+
 void setup() {
-	// initialize LED digital pin as an output.
-	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(pButt, INPUT_PULLUP);
+	pinMode(pTemp, INPUT);
+	pinMode(pLedOk, OUTPUT);
+	pinMode(pLedHeat, OUTPUT);
+	pinMode(pHeater, OUTPUT);
+	pinMode(pFan, OUTPUT);
+
+	u8g2.setI2CAddress(screenAddress); // Set the I2C address of the display
+	u8g2.begin();
 
 	// put your setup code here, to run once:
 	int result = myFunction(2, 3);
 }
 
 void loop() {
+	int val = analogRead(pTemp); // Read the temperature sensor value
+	u8g2.firstPage();
+	do {
+		u8g2.setFont(u8g2_font_luBS24_tr);
+		u8g2.setFontPosBottom();
+		u8g2.setCursor(0, 28);
+		u8g2.print(val);
+		u8g2.drawStr(0, 63, "( .Y. )"); // Draw the string at (0, 20)
+	} while (u8g2.nextPage());
+
+	delay(1000);
+
 	// turn the LED on (HIGH is the voltage level)
 	digitalWrite(LED_BUILTIN, HIGH);
 
