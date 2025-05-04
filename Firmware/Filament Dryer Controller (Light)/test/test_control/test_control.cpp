c87a598ee835c8256d09f3b2510d01019d30186c
halojenproductions <40622702+halojenproductions@users.noreply.github.com>
2025-05-04 16:08:18 +1200

    Started control tests.

---
+++ Firmware/Filament Dryer Controller (Light)/test/test_control/test_control.cpp
@@ -0,0 +1,18 @@
+#include "test_control.h"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void setup() {
+	delay(200);
+	UNITY_BEGIN();
+
+	// suite_control_dry::suite_Dry();
+	// suite_control_humid::suite_Humid();
+	// suite_control_error::suite_Error();
+
+	UNITY_END();
+}
+
+void loop() {}
---
+++ Firmware/Filament Dryer Controller (Light)/test/test_control/test_control.h
@@ -0,0 +1,36 @@
+#include "system.h"
+#include "utilities.h"
+#include <Arduino.h>
+#include <unity.h>
+
+#ifndef TEST_CONTROL_H
+#define TEST_CONTROL_H
+
+constexpr byte mask = 0b11111111;
+
+// namespace suite_control_dry {
+// 	void suite_Dry();
+// }
+
+// namespace suite_control_humid {
+// 	void suite_Humid();
+// }
+
+// namespace suite_control_error {
+// 	void suite_Error();
+// }
+
+
+
+void setUp(void);
+void tearDown(void);
+
+void setup();
+
+void loop();
+
+namespace test_control_helpers {
+
+}
+
+#endif
